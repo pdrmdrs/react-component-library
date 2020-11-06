ddaf5f836281991d0d4664157ddeccdaf1280a69
Pedro Medeiros <pdr.mdrs@gmail.com>
2020-11-06 20:26:50 -0300

    feat: adiciona OLA MUNDO na contact-screen

--- screens/contact-screen/src/contact-screen.tsx
+++ screens/contact-screen/src/contact-screen.tsx
@@ -5,7 +5,7 @@ interface IProps {}
 function ContactScreen(props: IProps): JSX.Element {
   return (
     <div>
-      <h1>Contact Screen</h1>
+      <h1>Contact Screen OLA MUNDO!</h1>
     </div>
   )
 }
