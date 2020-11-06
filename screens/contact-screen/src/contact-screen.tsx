5f094da5b3f257d39fd5e9500b901efb305f0d4a
Pedro Medeiros <pdr.mdrs@gmail.com>
2020-11-06 20:28:26 -0300

    feat: adiciona prop de contato

--- screens/contact-screen/src/contact-screen.tsx
+++ screens/contact-screen/src/contact-screen.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-interface IProps {}
+interface IProps {
+  contact?: number
+}
 
-function ContactScreen(props: IProps): JSX.Element {
+function ContactScreen({ contact }: IProps): JSX.Element {
   return (
     <div>
       <h1>Contact Screen OLA MUNDO!</h1>
+      {contact ? <h2>Contato: {contact}</h2> : null}
     </div>
   )
 }
