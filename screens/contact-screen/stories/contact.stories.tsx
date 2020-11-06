52e79360e5a0af0666afe8487e9c510c0500b9d8
Pedro Medeiros <pdr.mdrs@gmail.com>
2020-11-06 20:50:51 -0300

    docs(contact-screen): atualiza storybook com prop contact

--- screens/contact-screen/stories/contact.stories.tsx
+++ screens/contact-screen/stories/contact.stories.tsx
@@ -3,6 +3,8 @@ import ContactScreen from '../src'
 
 export const Primary = () => <ContactScreen />
 
+export const withContact = () => <ContactScreen contact={123123123} />
+
 export default {
   title: 'Contact Screen',
 }
