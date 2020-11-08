0bdcb223500d6ca65774e43395217d6c5253b976
Pedro Medeiros <pdr.mdrs@gmail.com>
2020-11-08 16:56:02 -0300

    refactor(dom-scripts): remove coisas sem uso

--- scripts/dom-scripts/__tests__/dom-scripts.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const domScripts = require('..');
-
-describe('@rcl/dom-scripts', () => {
-    it('needs tests');
-});
--- scripts/dom-scripts/webpack.prod.js
+++ scripts/dom-scripts/webpack.prod.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
-const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
