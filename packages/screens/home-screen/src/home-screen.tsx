4eb41b7ed5ecda7a397033f19f9eeab7dbedb458
Pedro Medeiros <pdr.mdrs@gmail.com>
2020-12-03 16:22:55 -0300

    feat(home-screen): add prop textoNaHome

--- packages/screens/home-screen/src/home-screen.tsx
+++ packages/screens/home-screen/src/home-screen.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 interface IProps {
   nome: string
+  textoNaHome: string
 }
 
-function HomeScreen({ nome }: IProps): JSX.Element {
+function HomeScreen({
+  nome,
+  textoNaHome = 'Ola monorepo',
+}: IProps): JSX.Element {
   return (
     <div style={{ color: 'blue' }}>
       {nome ? <h1>Home Screen {nome}</h1> : <h1>Sem nome</h1>}
+      <h2>{textoNaHome}</h2>
     </div>
   )
 }
